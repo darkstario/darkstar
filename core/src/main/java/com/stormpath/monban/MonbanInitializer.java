d1d971ae2b5ac1e4ccdf8ab2a7d2d3b5fe0f5524
Les Hazlewood <les@hazlewood.com>
2014-02-27 10:37:14 -0800

    disabled logging handler temporarily

--- core/src/main/java/com/stormpath/monban/MonbanInitializer.java
+++ core/src/main/java/com/stormpath/monban/MonbanInitializer.java
@@ -2,8 +2,8 @@ package com.stormpath.monban;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
+//import io.netty.handler.logging.LogLevel;
+//import io.netty.handler.logging.LoggingHandler;
 
 public class MonbanInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -18,7 +18,7 @@ public class MonbanInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     public void initChannel(SocketChannel ch) throws Exception {
         ch.pipeline().addLast(
-                new LoggingHandler(LogLevel.INFO),
+                //new LoggingHandler(LogLevel.INFO),
                 new FrontendHandler(remoteHost, remotePort));
     }
 }
